a8229c334d36507f6fefc09c7e85ad5ee40345a1
Michael Lee <mdlee@uci.edu>
2019-12-28 15:09:00 -0800

    Add files via upload

---
+++ code/highlightTrials.m
@@ -0,0 +1,16 @@
+function xTicks = highlightTrials(dataName, nTrials)
+%HIGHLIGHTTRIALS sets the tr[ial tick marks for displaying data sets
+%    xTicks = highlightTrials(dataName, nTrials)
+
+switch dataName
+   
+   case {'NewellShanksAll2003', 'NewellShanksHighCost2003', 'NewellShanksLowCost2003'}
+      xTicks = [1 60 nTrials];
+      
+   otherwise
+      xTicks = [1 nTrials];
+      
+      
+end
+
+
