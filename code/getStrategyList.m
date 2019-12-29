00bbdf427d789f2e9f694de48aca91360c55a02c
Michael Lee <mdlee@uci.edu>
2019-12-29 14:49:42 -0800

    Add files via upload

--- code/getStrategyList.m
+++ code/getStrategyList.m
@@ -1,23 +1,45 @@
-function [strategyList, paramsOne, paramsTwo] = getStrategyList(modelName)
+function [strategyList, paramsOne, paramsTwo, paramsThree] = getStrategyList(modelName)
 %GETSTRATEGYLIST Get the list of strategy names associated with a ss_ model
 % and the parameter list for the first switch-detection inference, and the
 % second strategy use and transition matrix inference
 %    [strategyList, paramsOne, paramsTwo] = getStrategyList(modelName)
 
 switch modelName
+   
    case 'ss_TTBTallyWADDGuess'
       strategyList = {'ttb', 'tally', 'wadd', 'guess'};
+      paramsOne = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
       paramsTwo = {'epsilon', 'z', 'pi', 'piPrime', 'choice'};
       
    case 'ss_GuessTTBTallyWADDWADDprobSaturated'
       strategyList = {'guess', 'ttb', 'tally', 'wadd', 'waddprob', 'saturated'};
+      paramsOne = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
       paramsTwo = {'epsilon', 'z', 'pi', 'piPrime'};
       
    case 'ss_TTBEQWTTBWADDGuess'
       strategyList = {'ttb', 'eqw-ttb', 'wadd', 'guess'};
+      paramsOne = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
       paramsTwo = {'epsilon', 'z', 'pi', 'piPrime', 'choice'};
+      
+   case 'ssLM_TTBTallyWADDGuess'
+      strategyList = {'ttb', 'tally', 'wadd', 'guess'};
+      paramsOne = {'x', 'phi'};
+      paramsTwo = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
+      paramsThree = {'epsilon', 'z', 'pi', 'piPrime', 'choice'};
+      
+   case 'ssLM_GuessTTBTallyWADDWADDprobSaturated'
+      strategyList = {'guess', 'ttb', 'tally', 'wadd', 'waddprob', 'saturated'};
+      paramsOne = {'x', 'phi'};
+      paramsTwo = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
+      paramsThree = {'epsilon', 'z', 'pi', 'piPrime'};
+      
+   case 'ssLM_TTBEQWTTBWADDGuess'
+      strategyList = {'ttb', 'eqw-ttb', 'wadd', 'guess'};
+      paramsOne = {'x', 'phi'};
+      paramsTwo = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
+      paramsThree = {'epsilon', 'z', 'pi', 'piPrime', 'choice'};
       
       
 end
 
-paramsOne = {'tau', 'gamma', 'muGamma', 'sigmaGamma'};
+
